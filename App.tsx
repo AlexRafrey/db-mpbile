9598f70b47190fb34c62000a6663cf236a1b87bf
Alex Shulgin <ashulgin@rutube.ru>
2022-11-20 08:53:24 +0300

    web iew

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+// import useColorScheme from "./hooks/useColorScheme";
+import WebView from "react-native-webview";
+import { Text } from "react-native";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <WebView
+          source={{ uri: "http://192.168.1.65:3000/" }}
+          onLoadProgress={({ nativeEvent }) => {
+            if (nativeEvent.progress === 1) {
+              console.log(3, "loaded");
+            }
+          }}
+          startInLoadingState={true}
+          renderLoading={() => <Text>Loadddddingggg</Text>}
+        />
+        {/*<Navigation colorScheme={colorScheme} />*/}
+        {/*<StatusBar />*/}
       </SafeAreaProvider>
     );
   }
---
+++ README.md
@@ -0,0 +1,5 @@
+dev: https://reactnative.dev/docs/environment-setup
+
+build: https://docs.expo.dev/classic/building-standalone-apps/
+
+web-view: https://github.com/react-native-webview/react-native-webview/blob/master/docs/Reference.md#renderloading
